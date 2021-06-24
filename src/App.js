592fe212616e664bbbed2847555f97b4f39fa860
gilang aditya <madegilangaditya32@gmail.com>
2021-06-24 16:37:13 +0800

    finish Add task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import {useState} from 'react'
 
 const App = () => {
@@ -8,30 +9,46 @@ const App = () => {
         id:1,
         text:'Doctors appointment',
         day: 'Feb 5th at 2:30pm',
-        reminder: 'true',
+        reminder: true,
     },
     {
         id:2,
         text:'Meeting',
         day: 'Feb 6th at 2:30pm',
-        reminder: 'true',
+        reminder: true,
     },
     {
         id:3,
         text:'Shopping',
         day: 'Feb 7th at 2:30pm',
-        reminder: 'false',
+        reminder: false,
     }
   ])
+
+  // Add Task
+  const addTask = (task)=>{
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   // Delete Task
   const deleteTask = (id) =>{
     setTasks(tasks.filter((task)=> task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => 
+      task.id === id ? {...task, reminder: !task.reminder} : task
+    ))
+  }
+
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks Found'}
+      <AddTask onAdd={addTask}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks Found'}
     </div>
   )
 }
