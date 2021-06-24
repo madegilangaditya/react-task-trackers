41e14fb35aefb389e7fb6bc369166eb1504b7668
gilang aditya <madegilangaditya32@gmail.com>
2021-06-24 09:10:41 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import {useState} from 'react'
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id:1,
+        text:'Doctors appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: 'true',
+    },
+    {
+        id:2,
+        text:'Meeting',
+        day: 'Feb 6th at 2:30pm',
+        reminder: 'true',
+    },
+    {
+        id:3,
+        text:'Shopping',
+        day: 'Feb 7th at 2:30pm',
+        reminder: 'false',
+    }
+  ])
+  // Delete Task
+  const deleteTask = (id) =>{
+    setTasks(tasks.filter((task)=> task.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks Found'}
     </div>
-  );
+  )
 }
+
+// Using class
+// class App extends React.Component{
+//   render() {
+//     return <h1>Hello From class</h1>
+//   }
+// }
 
 export default App;
