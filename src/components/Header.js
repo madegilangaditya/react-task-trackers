8f4586f4743518dfdb9a1ac91f501d157a92cb1b
gilang aditya <madegilangaditya32@gmail.com>
2021-06-30 13:42:44 +0800

    finish react task tracker using jSON server

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
+import { useLocation } from 'react-router'
 
 const Header = ({title, onAdd, showAdd}) => {
+    const location = useLocation()
     
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color={!showAdd ? 'green' : 'red'} text={!showAdd ? 'Add' : 'Close'} onClick={onAdd}/>
+            {location.pathname === '/' && <Button color={!showAdd ? 'green' : 'red'} text={!showAdd ? 'Add' : 'Close'} onClick={onAdd}/>}
         </header>
     )
 }
